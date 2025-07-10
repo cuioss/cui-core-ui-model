ae73b9d629395ab5a1fc478da770da518fa82f80
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2025-07-10 12:09:24 +0200

    Formatting. license header

--- src/main/java/de/cuioss/uimodel/model/Gender.java
+++ src/main/java/de/cuioss/uimodel/model/Gender.java
@@ -1,12 +1,12 @@
-/*
- * Copyright 2023 the original author or authors.
- * <p>
+/**
+ * Copyright © 2025 CUI-OpenSource-Software (info@cuioss.de)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * https://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -154,7 +154,7 @@ public enum Gender implements LabelKeyProvider {
      * @return An immutable map of string keys to Gender values
      */
     private static Map<String, Gender> initMapping() {
-        final Map<String, Gender> result = new HashMap<>(6);
+        final Map<String, Gender> result = HashMap.newHashMap(6);
         result.put("m", MALE);
         result.put("male", MALE);
         result.put("f", FEMALE);
--- src/test/java/de/cuioss/uimodel/nameprovider/DisplayNameTest.java
+++ src/test/java/de/cuioss/uimodel/nameprovider/DisplayNameTest.java
@@ -1,12 +1,12 @@
-/*
- * Copyright 2023 the original author or authors.
- * <p>
+/**
+ * Copyright © 2025 CUI-OpenSource-Software (info@cuioss.de)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * https://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,7 +43,7 @@ class DisplayNameTest extends ValueObjectTest<de.cuioss.uimodel.nameprovider.Dis
                 new de.cuioss.uimodel.nameprovider.DisplayName("First"),
                 new de.cuioss.uimodel.nameprovider.DisplayName("Second")
         );
-        assertEquals("First", names.get(0).getContent(), "First item content should match");
+        assertEquals("First", names.getFirst().getContent(), "First item content should match");
         assertEquals("Second", names.get(1).getContent(), "Second item content should match");
 
         // Equality example
